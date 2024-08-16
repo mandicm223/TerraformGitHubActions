cd911e7b7a4298ee8a1b2715eedabab700b206d7
markomandic <markomandic@pvh.com>
2024-08-16 11:11:50 +0200

    returning back codedeploy

--- code-deploy.tf
+++ code-deploy.tf
@@ -1,37 +1,37 @@
-# resource "aws_codedeploy_app" "application" {
-#   name             = "application"
-#   compute_platform = "ECS"
-# }
+resource "aws_codedeploy_app" "application" {
+  name             = "application"
+  compute_platform = "ECS"
+}
 
-# resource "aws_codedeploy_deployment_group" "deployment_group" {
-#   app_name              = aws_codedeploy_app.application.name
-#   deployment_group_name = "deployment-group"
-#   service_role_arn      = aws_iam_role.codedeploy_service_role.arn
+resource "aws_codedeploy_deployment_group" "deployment_group" {
+  app_name              = aws_codedeploy_app.application.name
+  deployment_group_name = "deployment-group"
+  service_role_arn      = aws_iam_role.codedeploy_service_role.arn
 
-#   blue_green_deployment_config {
-#     deployment_ready_option {
-#       action_on_timeout = "CONTINUE_DEPLOYMENT"
-#     }
+  blue_green_deployment_config {
+    deployment_ready_option {
+      action_on_timeout = "CONTINUE_DEPLOYMENT"
+    }
 
-#     terminate_blue_instances_on_deployment_success {
-#       action                           = "TERMINATE"
-#       termination_wait_time_in_minutes = 5
-#     }
-#   }
+    terminate_blue_instances_on_deployment_success {
+      action                           = "TERMINATE"
+      termination_wait_time_in_minutes = 5
+    }
+  }
 
-#   deployment_style {
-#     deployment_type   = "BLUE_GREEN"
-#     deployment_option = "WITH_TRAFFIC_CONTROL"
-#   }
+  deployment_style {
+    deployment_type   = "BLUE_GREEN"
+    deployment_option = "WITH_TRAFFIC_CONTROL"
+  }
 
-#   ecs_service {
-#     cluster_name = aws_ecs_cluster.main.name
-#     service_name = aws_ecs_service.main.name
-#   }
+  ecs_service {
+    cluster_name = aws_ecs_cluster.main.name
+    service_name = aws_ecs_service.main.name
+  }
 
-#   load_balancer_info {
-#     target_group_info {
-#       name = "your-target-group-name"
-#     }
-#   }
-# }
+  load_balancer_info {
+    target_group_info {
+      name = "your-target-group-name"
+    }
+  }
+}
