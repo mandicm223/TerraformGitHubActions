9ffc0137f5d561703bc20ee402ce4705bc0676c7
markomandic <markomandic@pvh.com>
2024-08-16 13:45:32 +0200

    formating

--- code-deploy.tf
+++ code-deploy.tf
@@ -4,11 +4,11 @@ resource "aws_codedeploy_app" "application" {
 }
 
 resource "aws_codedeploy_deployment_group" "deployment_group" {
-  app_name              = aws_codedeploy_app.application.name
-  deployment_group_name = "deployment-group"
-  service_role_arn      = aws_iam_role.codedeploy_service_role.arn
+  app_name               = aws_codedeploy_app.application.name
+  deployment_group_name  = "deployment-group"
+  service_role_arn       = aws_iam_role.codedeploy_service_role.arn
   deployment_config_name = "CodeDeployDefault.ECSAllAtOnce"
-  
+
 
   deployment_style {
     deployment_type   = "BLUE_GREEN"
