906ef9a9889eb420e60e2ce0dd687e8692f73177
markomandic <markomandic@pvh.com>
2024-08-19 23:44:37 +0200

    trying to create redis

--- elasticache.tf
+++ elasticache.tf
@@ -11,7 +11,6 @@ resource "aws_elasticache_replication_group" "redis" {
   replication_group_id       = "my-redis-cluster"
   description                = "Redis replication group with cluster mode enabled"
   node_type                  = "cache.t2.micro"
-  num_cache_clusters         = 2 # Number of primary nodes
   automatic_failover_enabled = true
   engine                     = "redis"
   engine_version             = "7.1"
@@ -19,8 +18,20 @@ resource "aws_elasticache_replication_group" "redis" {
   port                       = 6379
   subnet_group_name          = aws_elasticache_subnet_group.redis_subnet_group.name
   security_group_ids         = [aws_security_group.redis_sg.id]
-  num_node_groups            = 2 # Number of shards
-  replicas_per_node_group    = 1 # Number of replicas per shard
+
+  apply_immediately          = true
+  auto_minor_version_upgrade = false
+  maintenance_window         = "tue:06:30-tue:07:30"
+  snapshot_window            = "01:00-02:00"
+
+  log_delivery_configuration {
+    destination      = aws_cloudwatch_log_group.redis_cache_log_group.name
+    destination_type = "cloudwatch-logs"
+    log_format       = "text"
+    log_type         = "slow-log"
+  }
+  num_node_groups         = 2 # Number of shards
+  replicas_per_node_group = 1 # Number of replicas per shard
 
   tags = {
     Name = "my-redis-cluster"
--- logs.tf
+++ logs.tf
@@ -11,3 +11,17 @@ resource "aws_cloudwatch_log_stream" "bff_service_log_stream" {
   name           = "bff-service-log-stream"
   log_group_name = aws_cloudwatch_log_group.bff_service_log_group.name
 }
+
+resource "aws_cloudwatch_log_group" "redis_cache_log_group" {
+  name              = "/cache/redis"
+  retention_in_days = 30
+
+  tags = {
+    Name = "redis-log-group"
+  }
+}
+
+resource "aws_cloudwatch_log_stream" "redis_cache_log_stream" {
+  name           = "redis-log-stream"
+  log_group_name = aws_cloudwatch_log_group.redis_cache_log_group.name
+}
