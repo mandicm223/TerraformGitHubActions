183d228291d8a4076c40ff5f12cf565a18d41547
markomandic <markomandic@pvh.com>
2024-08-19 23:33:51 +0200

    updated redis to cluster mode

--- elasticache.tf
+++ elasticache.tf
@@ -7,15 +7,21 @@ resource "aws_elasticache_subnet_group" "redis_subnet_group" {
   }
 }
 
-resource "aws_elasticache_cluster" "redis" {
-  cluster_id           = "asics-redis-cluster"
-  engine               = "redis"
-  node_type            = "cache.t3.micro"
-  num_cache_nodes      = 1
-  parameter_group_name = "default.redis3.2"
-  engine_version       = "3.2.10"
-  subnet_group_name    = aws_elasticache_subnet_group.redis_subnet_group.name
-  security_group_ids   = [aws_security_group.redis_sg.id]
+resource "aws_elasticache_replication_group" "redis" {
+  replication_group_id       = "my-redis-cluster"
+  description                = "Redis replication group with cluster mode enabled"
+  node_type                  = "cache.t2.micro"
+  num_cache_clusters         = 2 # Number of primary nodes
+  automatic_failover_enabled = true
+  engine                     = "redis"
+  engine_version             = "7.1"
+  parameter_group_name       = "default.redis7.cluster.on"
+  port                       = 6379
+  subnet_group_name          = aws_elasticache_subnet_group.redis_subnet_group.name
+  security_group_ids         = [aws_security_group.redis_sg.id]
+  cluster_mode               = enabled
+  num_node_groups            = 2 # Number of shards
+  replicas_per_node_group    = 1 # Number of replicas per shard
 
   tags = {
     Name = "my-redis-cluster"
