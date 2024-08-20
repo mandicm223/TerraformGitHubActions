b4af36b1998eefeaddffc61253270ae776bd7705
markomandic <markomandic@pvh.com>
2024-08-20 14:10:03 +0200

    testing new listener setup

--- alb.tf
+++ alb.tf
@@ -1,6 +1,7 @@
 resource "aws_lb" "main" {
   name               = "main"
   load_balancer_type = "application"
+  internal           = true
   subnets            = aws_subnet.private.*.id
   idle_timeout       = 60
   security_groups    = [aws_security_group.lb.id]
@@ -8,8 +9,8 @@ resource "aws_lb" "main" {
 
 # Target Group for Blue Environment (Current Version)
 resource "aws_lb_target_group" "bff_service_blue_tg" {
-  name        = "bff-service-blue-target-group"
-  port        = 80
+  name        = "bff-service-tg"
+  port        = 8081
   protocol    = "HTTP"
   vpc_id      = aws_vpc.main.id
   target_type = "ip"
@@ -21,7 +22,7 @@ resource "aws_lb_target_group" "bff_service_blue_tg" {
     matcher             = "200"
     timeout             = "30"
     path                = "/health"
-    unhealthy_threshold = "5"
+    unhealthy_threshold = "2"
   }
 }
 
@@ -47,7 +48,7 @@ resource "aws_lb_target_group" "bff_service_blue_tg" {
 # Listener for Development Traffic
 resource "aws_lb_listener" "dev_listener" {
   load_balancer_arn = aws_lb.main.arn
-  port              = "80"
+  port              = "8081"
   protocol          = "HTTP"
 
   default_action {
--- network.tf
+++ network.tf
@@ -59,9 +59,9 @@ resource "aws_route" "internet_access" {
 #     }
 # }
 
-# # Explicitly associate the newly created route tables to the private subnets (so they don't default to the main route table)
-# resource "aws_route_table_association" "private" {
-#   count          = var.az_count
-#   subnet_id      = element(aws_subnet.private.*.id, count.index)
-#   route_table_id = element(aws_route_table.private.*.id, count.index)
-# }
+# Explicitly associate the newly created route tables to the private subnets (so they don't default to the main route table)
+resource "aws_route_table_association" "private" {
+  count          = var.az_count
+  subnet_id      = element(aws_subnet.private.*.id, count.index)
+  route_table_id = element(aws_route_table.private.*.id, count.index)
+}
