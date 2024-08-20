8674afbc886e92d42000fd01b655d6923b3a3c73
markomandic <markomandic@pvh.com>
2024-08-20 16:25:54 +0200

    fixed reference in redis sg

--- security.tf
+++ security.tf
@@ -111,7 +111,7 @@ resource "aws_security_group" "redis_sg" {
     from_port       = 6379
     to_port         = 6379
     protocol        = "tcp"
-    security_groups = [aws_security_group.ecs_tasks.id]
+    security_groups = [aws_security_group.bff_ecs_tasks.id]
     description     = "Allow ECS service access to Redis"
   }
 
