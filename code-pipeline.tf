46c473ee25d1d82ee7c0a8be0e6c77a949cad5f4
markomandic <markomandic@pvh.com>
2024-09-04 16:56:09 +0200

    trying with version 1 again

--- code-pipeline.tf
+++ code-pipeline.tf
@@ -19,7 +19,7 @@ resource "aws_codepipeline" "codepipeline" {
       category         = "Source"
       owner            = "AWS"
       provider         = "CodeStarSourceConnection"
-      version          = "2"
+      version          = "1"
       output_artifacts = ["source_output"]
       configuration = {
         ConnectionArn    = aws_codestarconnections_connection.github_connection.arn
