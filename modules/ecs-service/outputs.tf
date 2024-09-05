7d9c0fc216632738d408a0b9545b15a2e9ce16aa
markomandic <markomandic@pvh.com>
2024-09-05 16:16:42 +0200

    fixed unsoported argument

--- modules/ecs-service/outputs.tf
+++ modules/ecs-service/outputs.tf
@@ -1,7 +1,7 @@
 output "task_definition_arns" {
-  value = aws_ecs_task_definition.task[*].arn
+  value = aws_ecs_task_definition.task.arn
 }
 
 output "service_arns" {
-  value = aws_ecs_service.service[*].arn
+  value = aws_ecs_service.service.arn
 }
