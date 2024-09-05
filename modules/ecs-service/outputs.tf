c7670802d278d1d7261fa0d0e44fcb7cbd75ff75
markomandic <markomandic@pvh.com>
2024-09-05 16:22:00 +0200

    fixed syntax

--- modules/ecs-service/outputs.tf
+++ modules/ecs-service/outputs.tf
@@ -1,7 +1,7 @@
 output "task_definition_arns" {
-  value = aws_ecs_task_definition.task.arn[count.index]
+  value = aws_ecs_task_definition.task[count.index].arn
 }
 
 output "service_arns" {
-  value = aws_ecs_service.service.arn[count.index]
+  value = aws_ecs_service.service[count.index].arn
 }
