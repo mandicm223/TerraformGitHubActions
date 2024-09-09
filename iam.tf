281b9f4f1bad7a0988091a815666033a8e90b642
markomandic <markomandic@pvh.com>
2024-09-09 15:22:28 +0200

    added second arg to format()

--- iam.tf
+++ iam.tf
@@ -1,6 +1,6 @@
 locals {
   asics_auth_client_secret     = format("arn:aws:ssm:eu-west-1:%s:parameter/ecs/%s/asics_auth_client_secret", var.account_ids, local.gtw_service_name)
-  conntentstack_delivery_token = format("arn:aws:ssm:eu-west-1:%s:parameter/ecs/%s/conntentstack_delivery_token", local.bff_service_name)
+  conntentstack_delivery_token = format("arn:aws:ssm:eu-west-1:%s:parameter/ecs/%s/conntentstack_delivery_token", var.account_ids, local.bff_service_name)
 }
 
 module "iam" {
